c77be3cf1d28e5c9298b5f3ffe54d1b469ff7a9d
fletcherdeal92 <fletcherdeal@gmail.com>
2022-07-10 18:45:29 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Fletcher Deal on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
